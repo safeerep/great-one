045ba8463a0a6535ceef8680fe8bcbaf5c324552
safeerep <safeerep77@gmail.com>
2024-01-07 11:47:16 +0530

    first from product service

---
+++ services/product-service/src/index.ts
@@ -0,0 +1,9 @@
+import start from './start'
+
+( async () => {
+    try {
+        start;
+    } catch (error) {
+        console.log(error);
+    }
+})()
---
+++ services/product-service/src/start.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express, { Express, Request, Response } from "express";
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const app: Express = express();
+const PORT: number = Number(process.env.PORT) || 3002;
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true
+}
+const SessionOptions = {
+    secret: String(process.env.SESSION_SECRET),
+    resave: true,
+    saveUninitialized: true
+}
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+app.use(session(SessionOptions))
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('yes safeer product service is fine')
+})
+
+// app.use('/api/product')
+
+app.listen( PORT, () => {
+    console.log(`product-service is running at the port ${PORT}`);
+})
+
+export default app;
